67edba72cb394181ef30a0f6ebbd42cf582d6747
JEunseo <jjeseo1010@gmail.com>
2024-08-02 21:38:12 +0900

    Update ReplyList.js
    0802 21:38

--- Hotel-Project/Hotel-react/src/reply/ReplyList.js
+++ Hotel-Project/Hotel-react/src/reply/ReplyList.js
@@ -1,27 +1,51 @@
 import {Button, Container, Table} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
 
 let ReplyList = () => {
     let [data, setData] = useState({replyList: []})
     let navigate = useNavigate();
 
+    // 추가
+    let params = useParams()
+    let id = parseInt(params.id)
+    let location = useLocation()
+    // user 정보가 없기 때문에 강제값으로 '1' 넣음
+    let userInfo = location.state && location.state.userInfo ? location.state.userInfo : {id:1};
 
-    useEffect ( () => {
-        let selectList = async() => {
+
+    useEffect(() => {
+        let selectList = async () => {
             // 이부분에 메인에서 넘어오는 호텔 Id 를 받아오도록 연결 시에 수정해야함
+            // => [추가] Params로 id 값 받아옴
             let resp = await axios
                 .get('http://localhost:8080/reply/selectList');
 
             console.log(resp.data)
 
-            if(resp.status === 200){
+            if (resp.status === 200) {
                 setData(resp.data)
             }
         }
         selectList();
     }, [])
+
+    let moveToWrite = () => {
+        navigate('/reply/write')
+    }
+    // 추가 (수정 & 삭제)
+    let onUpdate = (id) => {
+        navigate('/reply/update/' + id, {state: {userInfo: userInfo}})
+    }
+    let onDelete = async (id) => {
+        let resp = await axios.get('http://localhost:8080/reply/delete/' + id, {
+            withCredentials: true
+        })
+        if (resp.status === 200) {
+            navigate('/reply/showList', {state: {userInfo: userInfo}})
+        }
+    }
 
 // ----------------- Table -------------------------
 
@@ -32,12 +56,18 @@ let ReplyList = () => {
                 <td>{reply.title}</td>
                 <td>{reply.content}</td>
                 <td>{reply.entryDate}</td>
+                {reply.customerId === userInfo.id?
+                    <tr>
+                        <td>
+                            <Button onClick={() => onUpdate(reply.id)}>수정</Button>
+                        </td>
+                        <td>
+                            <Button onClick={() =>onDelete(reply.id)}>삭제</Button>
+                        </td>
+                    </tr>
+                    :null}
             </tr>
         )
-    }
-
-    let moveToWrite = () => {
-        navigate('/reply/write')
     }
 
     return (
@@ -55,6 +85,7 @@ let ReplyList = () => {
                     <th>content</th>
                     <th>리뷰 게시 날짜</th>
                     <th></th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -64,7 +95,6 @@ let ReplyList = () => {
                 </tbody>
             </Table>
         </Container>
-
     )
 }
 
