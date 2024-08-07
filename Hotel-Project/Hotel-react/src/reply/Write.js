3bad5a04b872ab4e63808cee7ce3541756fbc7f8
JEunseo <jjeseo1010@gmail.com>
2024-08-07 21:19:56 +0900

    Update Write.js
    0807 21:19

--- Hotel-Project/Hotel-react/src/reply/Write.js
+++ Hotel-Project/Hotel-react/src/reply/Write.js
@@ -1,15 +1,131 @@
-import {Container} from "react-bootstrap";
-
+import React, {useEffect, useState} from "react";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import {Button, Table, Container} from "react-bootstrap";
+import {FaStar} from "react-icons/fa";
+import axios from "axios";
 
 let Write = () => {
-    return(
-        <Container>
-            <form>
+    let {hotelId} = useParams()
+    let [inputs, setInputs] = useState({
+        id: '',
+        hotelId: hotelId,
+        customerId: '',
+        title: '',
+        score: '',
+        content: ''
+    })
+    let location = useLocation()
+    let userInfo = location.state?.userInfo || {id:null}
+    let navigate = useNavigate()
 
+    let [message, setMessage] = useState('')
+
+    let ARRAY = [1, 2, 3, 4, 5];
+    let [score, setScore] = useState([false, false, false, false, false])
+
+    let moveToNext = () => {
+        let hotelId = inputs.hotelId
+        console.log("hotelID: ", hotelId)
+        navigate('/reply/replyList2/' + hotelId, {state: {userInfo: userInfo}})
+    }
+
+    let starScore = (index) => {
+        let newScore = ARRAY.map((_, i) => i < index);
+        setScore(newScore);
+        setInputs({
+            ...inputs,
+            score: index
+        })
+    }
+
+    useEffect(() => {
+        if (inputs.score >= 3) {
+            setMessage('어떤 점이 만족스러웠나요?')
+        } else {
+            setMessage('어떤 점이 아쉬웠나요?')
+        }
+    }, [inputs.score])
+
+    {
+        ARRAY.map((el, index) => (
+            <FaStar key={index} size="14"></FaStar>
+        ))
+    }
+
+    let onChange = (e) => {
+        let {name, value} = e.target
+        setInputs({
+            ...inputs,
+            [name]: value
+        })
+    }
+    let onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let resp = await axios.post('http://localhost:8080/reply/write', inputs, {
+                withCredentials: true
+            })
+            console.log("resp data: ", resp.data)
+            if (resp.status === 200) {
+                moveToNext()
+            }
+        } catch (error) {
+            console.error("Error: ", error)
+        }
+    }
+
+    return (
+        <Container classname={"mt-3"}>
+            <form onSubmit={onSubmit}>
+                <Table striped>
+                    <thead>
+                    <tr>
+                        <td>[리뷰 작성하기]</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>호텔은 만족하셨나요?</td>
+                        <td>
+                            {ARRAY.map((el, index) => (
+                                <FaStar key={index} size="24"
+                                        onClick={() => starScore(el)}
+                                        color={score[index] ? "#ffc107" : "#e4e5e9"}
+                                        style={{cursor: 'pointer'}}/>
+                            ))}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} className={'text-center'}>
+                            만족도 {inputs.score}점을 주셨네요.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} className={'text-center'}>{message}</td>
+                    </tr>
+                    <tr>
+                        <td>제목</td>
+                        <td>
+                            <input type={'text'} value={inputs.title} name={'title'} onChange={onChange}>
+                            </input>
+                        </td>
+                    </tr>
+                    {/*<td>내용</td>*/}
+                    <tr>
+                        <td colSpan={2}>
+                <textarea name={'content'} value={inputs.content} className={'form-control'} onChange={onChange}>
+                </textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} className={'text-center'}>
+                            <Button type={'submit'}>등록하기</Button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </Table>
             </form>
         </Container>
-
     )
 }
-
-export default Write;
+export default Write
