41934d6f9ea162456d247fc818bf0809866ad7ef
JEunseo <jjeseo1010@gmail.com>
2024-08-07 21:26:21 +0900

    Update App.js
    0807 21:26

--- Hotel-Project/Hotel-react/src/App.js
+++ Hotel-Project/Hotel-react/src/App.js
@@ -1,14 +1,21 @@
-
+import React from "react";
 import './App.css';
 import {Route, Routes} from "react-router-dom";
+import Write from "./reply/Write";
+import Update from "./reply/Update";
 import ReplyList from "./reply/ReplyList";
+import ReplyList2 from "./reply/ReplyList2";
 
 function App() {
   return (
-    <Routes>
-        {/* hotel 의 id 값을 받아와야 함 */}
-        <Route path={"/reply/selectList/"} element={<ReplyList/>}/>
-    </Routes>
+    <div>
+      <Routes>
+          <Route path={"/reply/write/:hotelId"} element={<Write/>}/>
+          <Route path={"/reply/update/:id"} element={<Update/>}/>
+          <Route path={"/reply/replyList"} element={<ReplyList/>}/>
+          <Route path={"/reply/replyList2/:hotelId"} element={<ReplyList2/>}/>
+      </Routes>
+    </div>
   );
 }
 
